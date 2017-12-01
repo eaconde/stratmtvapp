139833d291ab9c2caddd70af691209a6e9f82f2b
Eric Conde <econde@stratpoint.com>
2017-12-01 14:30:16 +0800

    gemset and rubyversion

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @wishes = [
      { pic: 'eric_conde.jpg', details: 'World peace', status: 'PENDING' },
-     { pic: 'eric_conde.jpg', details: 'World peace 2', status: 'PENDING' },
+     { pic: 'tin_villaruel.jpg', details: 'World peace 2', status: 'PENDING' },
      { pic: 'eric_conde.jpg', details: 'World peace 3', status: 'PENDING' }
     ]
 
